5e65fecb53485e9bd56a438bef0121c8cad2f47b
ivan-mp <ghkatende@gmail.com>
2021-01-09 14:49:01 +0300

    computed the unique recipe count

--- Processor.php
+++ Processor.php
@@ -13,15 +13,44 @@ class Processor
     {
     }
 
+
+    public function cleanUp($data)
+    {
+        $clean = array();
+        foreach ($data as $key => $count) {
+            $recipe = str_replace('_', ' ', $key);
+            array_push($clean, array("recipe" => $recipe, "count" => $count));
+        }
+        return $clean;
+    }
+
+
     public function get_count_per_recipe()
     {
-        return $this->data;
+        $dictionary = array();
+        foreach ($this->data as $recipe) {
+            $recipe_name = $recipe['recipe'];
+            $key = str_replace(' ', '_', $recipe_name);
+            if (array_key_exists($key, $dictionary)) {
+                $dictionary[$key] = (int) $dictionary[$key] + 1;
+            } else {
+                $dictionary[$key] = 1;
+            }
+        }
+        return $this->cleanUp($dictionary);
     }
 
 
     public function get_unique_recipe_count()
     {
-        return $this->data;
+        $unique =  0;
+        $tallied_recipes = $this->get_count_per_recipe();
+        foreach ($tallied_recipes  as $recipe) {
+            if ($recipe['count'] === 1) {
+                $unique += 1;
+            }
+        }
+        return array("unique_recipe_count" => $unique);
     }
 
     public function get_busiest_postcode()
--- index.php
+++ index.php
@@ -4,3 +4,30 @@ $data_reference = file_get_contents("data.json");
 $data = json_decode($data_reference, true);
 
 $processor = new Processor($data);
+
+?>
+
+
+<html>
+
+<body>
+    <h1>Count per recipe</h1>
+    <pre>
+    <?php
+    print_r($processor->get_count_per_recipe());
+
+    ?>
+</pre>
+
+
+    <h1>Count per recipe</h1>
+    <pre>
+    <?php
+    print_r($processor->get_unique_recipe_count());
+
+    ?>
+</pre>
+
+</body>
+
+</html>
